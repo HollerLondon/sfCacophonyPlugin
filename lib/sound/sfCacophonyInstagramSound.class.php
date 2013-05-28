c0e831d3b366b333476289e1cfd2c0c6ba292248
Jo Carter <github@jocarter.co.uk>
2013-05-28 10:10:30 -0700

    Adding profile image URL to returned data

--- lib/sound/sfCacophonyInstagramSound.class.php
+++ lib/sound/sfCacophonyInstagramSound.class.php
@@ -60,6 +60,7 @@ class sfCacophonyInstagramSound
     $user['normalized']['providers_user_id'] = $userRaw['id'];
     $user['normalized']['username']          = $userRaw['username'];
     $user['normalized']['full_name']         = $userRaw['full_name'];
+    $user['normalized']['profile_picture']   = $userRaw['profile_picture'];
     
     if (isset($userRaw['full_name']) && $userRaw['full_name'] != $userRaw['username'])
     {
